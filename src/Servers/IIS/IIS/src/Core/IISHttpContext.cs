cda806970db136b3efd28f8b0a1861c1c13c0da4
Justin Kotalik <jukotali@microsoft.com>
2019-04-17 14:15:58 -0700

    nits

--- src/Servers/IIS/IIS/src/Core/IISHttpContext.cs
+++ src/Servers/IIS/IIS/src/Core/IISHttpContext.cs
@@ -352,7 +352,7 @@ namespace Microsoft.AspNetCore.Server.IIS.Core
         private void SetErrorResponseHeaders(int statusCode)
         {
             StatusCode = statusCode;
-            ReasonPhrase = string.Empty; // TODO
+            ReasonPhrase = string.Empty;
             HttpResponseHeaders.Clear();
         }
 
@@ -479,7 +479,7 @@ namespace Microsoft.AspNetCore.Server.IIS.Core
 
         public void SetBadRequestState(BadHttpRequestException ex)
         {
-            //Log.ConnectionBadRequest(ConnectionId, ex);
+            Log.ConnectionBadRequest(_logger, RequestConnectionId, ex);
 
             if (!HasResponseStarted)
             {
